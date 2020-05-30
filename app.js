06d1f626634d6831b64c667b77a5991f788461c3
KhaiNguyen <khainbgch17459@fpt.edu.vn>
2020-05-30 20:43:04 +0700

    first

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const mysql = require('mysql');
+const app = express();
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'web'
+});
+
+connection.connect(function(err){
+  (err) ? console.log(err) : console.log(connection);
+});
+
+app.get('/api/news', (req, res) => {
+  var sql = "SELECT * FROM account ORDER BY accountid DESC";
+  connection.query(sql, function(err, results) {
+    if (err) throw err;
+    res.json({news: results});
+  });
+});
+
+app.listen(4000, () => console.log('App listening on port 4000'));
